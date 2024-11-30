93e8972c29622578181c72b5e4bc3f39997fd9d2
Josh Gochey <josh@gochey.net>
2024-11-29 22:35:06 -0600

    More tweaks to the forecast page. Location type dropdown should be working.

--- src/app/NewForecastLocation/new/page.js
+++ src/app/NewForecastLocation/new/page.js
@@ -2,11 +2,5 @@ import React from 'react';
 import NewLocationForm from '../../../components/forms/NewLocationForm';
 
 export default function NewLocation() {
-  // const { firebaseKey } = params;
   return <NewLocationForm />;
 }
-// return <NewLocationForm obj={firebaseKey} />;
-
-// NewLocation.propTypes = {
-//   params: PropTypes.objectOf({}).isRequired,
-// };
--- src/components/ForecastCard.js
+++ src/components/ForecastCard.js
@@ -1,15 +1,50 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Link from 'next/link';
-// import { Dropdown } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
+import { getUserLocations } from '../api/userData';
+import { useAuth } from '../utils/context/authContext';
+import { getForecastsAlternate } from '../api/forecastData';
 
 // const userId = firebaseKey-- The userId used for CRUD should be pulled from the user's firebaseId.
 
-export default function ForecastCard({ UserId, forecastObj, LocationName, LocationType }) {
+// export default function ForecastCard({ UserId, forecastObj, LocationName, LocationType }) {
+// export default function ForecastCard({ UserId, forecastObj }) {
+export default function ForecastCard({ UserId }) {
+  const { user } = useAuth();
+  const [userLocations, setLocations] = useState([]);
+  const [currentLocationName, setCurrentLocationName] = useState(null);
+  const [currnetLocationType, setCurrentLocationType] = useState(null);
+  const [currentLocationId, setCurrentLocationId] = useState(null);
+  const [currentForecastInfo, setCurrentForecastInfo] = useState(null);
+
+  useEffect(() => {
+    if (user && user.uid) {
+      getUserLocations(user.uid).then((locations) => {
+        // This will make sure locations is an object for the dropdown menu.
+        if (locations && typeof locations === 'object') {
+          setLocations(locations);
+        } else {
+          setLocations({}); // Make sure its an object.
+        }
+      });
+    }
+  }, [user]);
+
+  const handleLocationSelect = (location) => {
+    setCurrentLocationName(location.name);
+    setCurrentLocationType(location.location_type);
+    setCurrentLocationId(location.id);
+    getForecastsAlternate(currentLocationId).then(setCurrentForecastInfo);
+  };
+
+  // Convert the userLocations object to an array for rendering in the dropdown
+  const locationArray = Object.values(userLocations);
+
   return (
     <div>
       <Link href={`/SavedLocations/${UserId}`} passHref>
@@ -18,19 +53,38 @@ export default function ForecastCard({ UserId, forecastObj, LocationName, Locati
         </Button>
       </Link>
 
-      <div>
+      {/* Saved Locations Dropdown Menu */}
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Select Location
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          {locationArray.length === 0 ? (
+            <Dropdown.Item disabled>No saved locations</Dropdown.Item>
+          ) : (
+            locationArray.map((location) => (
+              <Dropdown.Item key={location.id} onClick={() => handleLocationSelect(location)}>
+                {location.name}
+              </Dropdown.Item>
+            ))
+          )}
+        </Dropdown.Menu>
+      </Dropdown>
+
+      {/* <div>
         <h1>Forecast Data</h1>
         <ul>
           <Card style={{ width: '18rem', margin: '10px' }}>
             <Card.Body>
-              {forecastObj.map((forecast) => (
+              {currentForecastInfo.map((forecast) => (
                 <li key={forecast.id}>
                   <Card.Title>
-                    <p>Location Name: {LocationName}</p>
-                    <p>Location Type: {LocationType}</p>
+                    <p>Location Name: {currentLocationName}</p>
+                    <p>Location Type: {currnetLocationType}</p>
                   </Card.Title>
 
-                  {/* Rendering individual forecast properties instead of the whole object */}
+                  Rendering individual forecast properties instead of the whole object
                   <p>Date: {forecast.date}</p>
                   <p>Temperature: {forecast.temperature}°C</p>
                   <p>Humidity: {forecast.humidity}%</p>
@@ -45,47 +99,56 @@ export default function ForecastCard({ UserId, forecastObj, LocationName, Locati
         </ul>
       </div>
 
-      {/* Saved Locations Dropdown Menu goes here */}
-      {/* <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
+    </div>
+  );
+} */}
+      <div>
+        <h1>Forecast Data</h1>
+        {currentForecastInfo && Array.isArray(currentForecastInfo) && currentForecastInfo.length > 0 ? (
+          <ul>
+            <Card style={{ width: '18rem', margin: '10px' }}>
+              <Card.Body>
+                {currentForecastInfo.map((forecast) => (
+                  <li key={forecast.id}>
+                    <Card.Title>
+                      <p>Location Name: {currentLocationName}</p>
+                      <p>Location Type: {currnetLocationType}</p>
+                    </Card.Title>
 
-      <Dropdown.Menu>
-
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-
-        <Dropdown.Menu>
-            <div className="d-flex flex-wrap justify-content-md-center">
-              {tempList.map((genre) => (
-                // GIVE DATA
-                <Dropdown.Item onClick={() => submitClick(genre.id, genre.name)}> {genre.name} </Dropdown.Item>
-              ))}
-            </div>
-          </Dropdown.Menu>
-
-      </Dropdown.Menu>
-    </Dropdown>
- */}
+                    {/* Rendering individual forecast properties */}
+                    <p>Date: {forecast.date}</p>
+                    <p>Temperature: {forecast.temperature}°C</p>
+                    <p>Humidity: {forecast.humidity}%</p>
+                    <p>Chance of Rain: {forecast.chance_of_rain}%</p>
+                    <p>
+                      Icon: <img src={forecast.icon} alt="weather icon" />
+                    </p>
+                  </li>
+                ))}
+              </Card.Body>
+            </Card>
+          </ul>
+        ) : (
+          <p>Select a location to view the forecast data.</p>
+        )}
+      </div>
     </div>
   );
 }
 
 ForecastCard.propTypes = {
-  forecastObj: PropTypes.arrayOf(
-    PropTypes.shape({
-      podcastId: PropTypes.number,
-      id: PropTypes.number,
-      date: PropTypes.string,
-      temperature: PropTypes.number,
-      humidity: PropTypes.number,
-      chance_of_rain: PropTypes.number,
-      icon: PropTypes.string,
-    }),
-  ).isRequired,
+  // forecastObj: PropTypes.arrayOf(
+  //   PropTypes.shape({
+  //     podcastId: PropTypes.number,
+  //     id: PropTypes.number,
+  //     date: PropTypes.string,
+  //     temperature: PropTypes.number,
+  //     humidity: PropTypes.number,
+  //     chance_of_rain: PropTypes.number,
+  //     icon: PropTypes.string,
+  //   }),
+  // ).isRequired,
   UserId: PropTypes.string.isRequired,
-  LocationName: PropTypes.string.isRequired,
-  LocationType: PropTypes.number.isRequired,
+  // LocationName: PropTypes.string.isRequired,
+  // LocationType: PropTypes.number.isRequired,
 };
